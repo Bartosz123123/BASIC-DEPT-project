7bc4522a72739692e5856e3210763039005ef3de
Bartosz <bartosz.opalka10@wp.pl>
2023-03-16 12:58:57 +0100

    initiatives mobile 50%

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,17 @@
 const menu = document.querySelector('.menu-links');
 const menuBtn = document.querySelector('.btn');
 const closeMenuBtn = document.querySelector('.btn-close');
+const arrowBack = document.querySelector('.arrow-back');
+const initiativesBox = document.querySelector('.initiatives');
+const initiativesBtn = document.querySelector('.initiatives-btn');
+
+const showInitiativesMenu = () => {
+	initiativesBox.classList.add('active-initiatives');
+};
+
+const closeInitiativesMenu = () => {
+	initiativesBox.classList.remove('active-initiatives');
+};
 
 const closeMenu = () => {
 	menu.classList.remove('active-menu');
@@ -16,5 +27,7 @@ const handleMenu = () => {
 	menu.classList.remove('close-menu');
 };
 
+initiativesBtn.addEventListener('click', showInitiativesMenu);
+arrowBack.addEventListener('click', closeInitiativesMenu);
 menuBtn.addEventListener('click', handleMenu);
 closeMenuBtn.addEventListener('click', closeMenu);
