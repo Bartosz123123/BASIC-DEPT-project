4a564dc48d844aa691469559e6b0073f16e7473d
Bartosz <bartosz.opalka10@wp.pl>
2023-03-16 20:21:24 +0100

    80% initiatives desktop

--- src/js/main.js
+++ src/js/main.js
@@ -5,6 +5,11 @@ const arrowBack = document.querySelector('.arrow-back');
 const initiativesBox = document.querySelector('.initiatives');
 const initiativesBtn = document.querySelector('.initiatives-btn');
 
+const slider = document.querySelector('.swipe-box');
+let isDown = false;
+let startX;
+let scrollLeft;
+
 const showInitiativesMenu = () => {
 	initiativesBox.classList.add('active-initiatives');
 };
@@ -26,6 +31,30 @@ const handleMenu = () => {
 	menu.classList.add('active-menu');
 	menu.classList.remove('close-menu');
 };
+
+slider.addEventListener('mousedown', (e) => {
+	isDown = true;
+	startX = e.pageX - slider.offsetLeft;
+	scrollLeft = slider.scrollLeft;
+});
+
+slider.addEventListener('mouseleave', () => {
+	isDown = false;
+});
+
+slider.addEventListener('mouseup', () => {
+	isDown = false;
+});
+
+slider.addEventListener('mousemove', (e) => {
+	if (!isDown) return;
+	e.preventDefault();
+	const x = e.pageX - slider.offsetLeft;
+	const walk = x - startX;
+	slider.scrollLeft = scrollLeft - walk;
+});
+
+
 
 initiativesBtn.addEventListener('click', showInitiativesMenu);
 arrowBack.addEventListener('click', closeInitiativesMenu);
